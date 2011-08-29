76de13857a4029be43d0427e9a773511a19e5dc7
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2011-08-29 10:02:49 +0000

    Panic SW (GUI) Button sub-service added

---
+++ personal_safety/personal_safety.sw.panic/src/main/java/org/universAAL/Application/personal_safety/sw/panic/SCallee.java
@@ -0,0 +1,84 @@
+package org.universAAL.Application.personal_safety.sw.panic;
+
+import org.osgi.framework.BundleContext;
+import org.universAAL.Application.personal_safety.sw.panic.osgi.Activator;
+import org.universAAL.middleware.context.ContextEvent;
+import org.universAAL.middleware.owl.supply.LevelRating;
+import org.universAAL.middleware.service.CallStatus;
+import org.universAAL.middleware.service.ServiceCall;
+import org.universAAL.middleware.service.ServiceCallee;
+import org.universAAL.middleware.service.ServiceRequest;
+import org.universAAL.middleware.service.ServiceResponse;
+import org.universAAL.middleware.service.owl.InitialServiceDialog;
+import org.universAAL.middleware.service.owls.process.ProcessOutput;
+import org.universAAL.middleware.service.owls.profile.ServiceProfile;
+import org.universAAL.ontology.profile.User;
+import org.universAAL.ontology.risk.PanicButton;
+
+public class SCallee extends ServiceCallee{
+	
+	private static final String MY_URI = "http://ontology.universAAL.org/panic.owl#";
+	private static final String SERVICE_URI = MY_URI+"SW_BUTTON";
+	private static final String SERVICE_START_URI = MY_URI+"SW_BUTTON_PRESSED";
+	
+	private static PanicButton panic = new PanicButton(PanicButton.MY_URI+"SWPanic");
+	
+	private static final ServiceResponse failure = new ServiceResponse(
+			CallStatus.serviceSpecificFailure);
+	
+	public SCallee(BundleContext context){
+		super(context, getProfiles());
+	}
+	
+	protected SCallee(BundleContext context, ServiceProfile[] realizedServices) {
+		super(context, realizedServices);
+		panic.setActivated(false);
+	}
+
+	public void communicationChannelBroken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ServiceResponse handleCall(ServiceCall call) {
+		if (call == null){
+			failure.addOutput(new ProcessOutput(ServiceResponse.PROP_SERVICE_SPECIFIC_ERROR,"Null call!?!"));
+			return failure;
+		}
+		String operation = call.getProcessURI();
+		if (operation == null){
+			failure.addOutput(new ProcessOutput(ServiceResponse.PROP_SERVICE_SPECIFIC_ERROR,"Null operation!?!"));
+			return failure;
+		}
+		if (operation.startsWith(SERVICE_START_URI)){
+			return panicButton(call);
+		}
+		return null;
+	}
+
+	static ServiceProfile[] getProfiles() {
+		ServiceProfile prof = InitialServiceDialog.createInitialDialogProfile(
+				SERVICE_URI,
+				"http://www.upm.es", "Software Mapped Panic Button",
+				SERVICE_START_URI);
+		return new ServiceProfile[]{prof};
+	}
+	
+	private ServiceResponse panicButton(ServiceCall call) {
+		User user=null;
+		Object inputUser = call.getProperty(ServiceRequest.PROP_uAAL_INVOLVED_HUMAN_USER);
+		if ((inputUser == null)||!(inputUser instanceof User)){
+			failure.addOutput(new ProcessOutput(ServiceResponse.PROP_SERVICE_SPECIFIC_ERROR,"Invalid User Input!"));
+			return failure;
+		}else{
+			user=(User)inputUser;
+			panic.setPressedBy(user);
+			panic.setActivated(true);
+		}
+		
+		ContextEvent panicCE = new ContextEvent(panic, PanicButton.PROP_ACTIVATED);
+		Activator.cpublisher.publish(panicCE);
+		return new ServiceResponse(CallStatus.succeeded);
+	}
+}
+
