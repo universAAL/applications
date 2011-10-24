1875684ff53cb1c19c4f9144f6021f0a2b4cfb5d
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2011-10-24 13:37:50 +0000

    Initial Import for Health Manager (Main Component)
    shell for UI forms.

---
+++ health/health.manager/src/main/java/org/universAAL/AALapplication/health/manager/HealthManager.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright 2011 Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.AALapplication.health.manager;
+
+import org.osgi.framework.BundleContext;
+import org.universAAL.ontology.profile.User;
+
+public class HealthManager {
+
+	private static HealthManager singleton;
+	private static BundleContext context;
+	private ISubscriber isubcriber;
+	private OPublisher opublisher;	
+	private User user;
+	
+	private HealthManager() {
+		isubcriber = new ISubscriber(context);
+		opublisher = new OPublisher(context);
+	}
+	
+	public static HealthManager getInstance() {
+		if (singleton==null) {
+			singleton = new HealthManager();
+		}
+		return singleton;
+	}
+	
+	public static void setContext(BundleContext ctxt) {
+		context=ctxt;
+	}
+	public ISubscriber getIsubcriber() {
+		return isubcriber;
+	}
+	public OPublisher getOpublisher() {
+		return opublisher;
+	}
+
+	public User getUser() {
+		return user;
+	}
+	
+	
+}
---
+++ health/health.manager/src/main/java/org/universAAL/AALapplication/health/manager/ISubscriber.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright 2011 Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.AALapplication.health.manager;
+
+
+import java.util.TreeMap;
+
+import org.osgi.framework.BundleContext;
+import org.universAAL.AALapplication.health.manager.ui.InputListener;
+import org.universAAL.middleware.input.InputEvent;
+import org.universAAL.middleware.input.InputSubscriber;
+
+public class ISubscriber extends InputSubscriber{
+
+	private TreeMap<String, InputListener> inputMapper;
+	
+	protected ISubscriber(BundleContext context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void communicationChannelBroken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dialogAborted(String dialogID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleInputEvent(InputEvent event) {
+		/*
+		 * Delegate Handlement to subscribed UI
+		 */
+		inputMapper.get(event.getDialogID()).handleEvent(event);
+	}
+	
+	public void registerUI(String formID, InputListener listener) {
+		inputMapper.put(formID, listener);
+	}
+
+	public void unresgisterUI(String formID) {
+		inputMapper.remove(formID);
+	}
+}
