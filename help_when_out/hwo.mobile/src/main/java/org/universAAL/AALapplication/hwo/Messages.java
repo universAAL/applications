80196da2742d2967d769ead033b9e11a1a98b69d
Alvaro Fides <alfiva@itaca.upv.es>
2011-11-11 10:33:44 +0000

    Fixed i18n

--- help_when_out/hwo.mobile/src/main/java/org/universAAL/AALapplication/hwo/Messages.java
+++ help_when_out/hwo.mobile/src/main/java/org/universAAL/AALapplication/hwo/Messages.java
@@ -5,7 +5,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "org.universAAL.AALapplication.hwo_"+Locale.getDefault().getLanguage().toUpperCase(); //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.universAAL.AALapplication.hwo.messages_"+Locale.getDefault().getLanguage().toUpperCase(); //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(BUNDLE_NAME);
