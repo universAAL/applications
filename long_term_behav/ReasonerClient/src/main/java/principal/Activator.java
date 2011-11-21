626ec185f7d1969c5f9a944148773bcff2c227cb
Alvaro Fides <alfiva@itaca.upv.es>
2011-11-21 15:31:55 +0000

    example code

--- long_term_behav/ReasonerClient/src/main/java/principal/Activator.java
+++ long_term_behav/ReasonerClient/src/main/java/principal/Activator.java
@@ -6,6 +6,7 @@ import org.osgi.framework.BundleContext;
 import org.universAAL.middleware.service.DefaultServiceCaller;
 import org.universAAL.middleware.service.ServiceCaller;
 import org.universAAL.middleware.service.ServiceRequest;
+import org.universAAL.ontology.drools.Rule;
 import org.universAAL.ontology.drools.service.DroolsService;
 import org.universAAL.samples.service.utils.SimpleAdd;
 import org.universAAL.samples.service.utils.SimpleRequest;
@@ -18,43 +19,44 @@ public class Activator implements BundleActivator{
 		
 	
 		
-		String my_rule ="Rule";
+//		String my_rule ="Rule";
+//		
+//		// I create a default caller and i pass him the actual context
+//		caller = new DefaultServiceCaller(context);
+//		
+//		System.out.println("caller created.........");
+//		
+//		// I create a object service request
+//
+//		ServiceRequest req = new ServiceRequest(new DroolsService(),null);
+//		
+//		
+//		System.out.println("request created.........");
+//			
+//        SimpleRequest set = new SimpleRequest(new DroolsService());
+//		
+//		
+//		
+//		
+//		set.putArgument(new String[] { DroolsService.RULE }, new SimpleAdd(my_rule));
+//		
+//		
+//		req.addRequiredOutput(SCalleeProvidedService.SERVER_NAMESPACE
+//				+ "output_temp",
+//				new String[] { DroolsService.RULE});
+//		
+//	
+//		System.out.println("IM CALLING REASONER-ADDRULE");
+//		
+//		// I call the service
+//
+//		caller.call(req);
 		
-		// I create a default caller and i pass him the actual context
+	
 		caller = new DefaultServiceCaller(context);
-		
-		System.out.println("caller created.........");
-		
-		// I create a object service request
-
-		ServiceRequest req = new ServiceRequest(new DroolsService(),null);
-		
-		
-		System.out.println("request created.........");
-			
-        SimpleRequest set = new SimpleRequest(new DroolsService());
-		
-		
-		
-		
-		set.putArgument(new String[] { DroolsService.RULE }, new SimpleAdd(my_rule));
-		
-		
-		req.addRequiredOutput(SCalleeProvidedService.SERVER_NAMESPACE
-				+ "output_temp",
-				new String[] { DroolsService.RULE});
-		
-	
-		System.out.println("IM CALLING REASONER-ADDRULE");
-		
-		// I call the service
-
-		caller.call(req);
-		
-	
-		
-		
-		
+		SimpleRequest add = new SimpleRequest(new DroolsService());
+		add.putArgument(new String[] { DroolsService.RULE }, new SimpleAdd(new Rule("http://ontology.universAAL.org/MyServer.owl#myRule")));
+		caller.call(add);
 	}
 
 	
