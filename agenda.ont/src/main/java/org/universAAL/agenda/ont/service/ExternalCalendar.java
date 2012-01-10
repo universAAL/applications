1ee966f93b2c75cef6fb3780f341b3b5e2ab9928
mr Andrej Grguric <andrej.grguric@ericsson.com>
2012-01-10 13:22:29 +0000

    tagging agenda mw 0.3.0 compatible (used for prototype)

---
+++ agenda.ont/src/main/java/org/universAAL/agenda/ont/service/ExternalCalendar.java
@@ -0,0 +1,13 @@
+package org.universAAL.agenda.ont.service;
+
+import org.universAAL.agenda.ont.Calendar;
+
+/**
+ * @author kagnantis
+ * 
+ */
+public interface ExternalCalendar {
+    public static final String MY_URI = Calendar.CALENDAR_NAMESPACE
+	    + "externalCalendar";
+    // do nothing at the moment
+}
