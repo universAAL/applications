81f4437661774f0d2af3c4fa7f7b8a7fbd2e251d
mr Andrej Grguric <andrej.grguric@ericsson.com>
2012-01-13 16:36:31 +0000

    agendaEventSelection.server platform 1.0.0 compatible

---
+++ agenda/agendaEventSelection.server/src/main/java/org/universAAL/agendaEventSelectionTool/database/EventSelectionToolDBInterface.java
@@ -0,0 +1,13 @@
+package org.universAAL.agendaEventSelectionTool.database;
+
+import java.util.List;
+
+import org.universAAL.agendaEventSelectionTool.ont.FilterParams;
+
+public interface EventSelectionToolDBInterface {
+	List requestEvents(FilterParams filterParams);
+	List requestFromCalendarEvents(FilterParams filterParams, List calendarList);
+	List requestFromCalendarLimitedEvents(FilterParams filterParams, List calendarList, int maxEventNo);
+	List requestFollowingEvents(List calendarList, int maxEventNo);
+	
+}
