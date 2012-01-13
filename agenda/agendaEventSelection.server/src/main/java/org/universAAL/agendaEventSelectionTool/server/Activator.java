1ece35b6547b6f064f184e369c40d2ac12e4f291
mr Andrej Grguric <andrej.grguric@ericsson.com>
2012-01-13 16:36:31 +0000

    agendaEventSelection.server platform 1.0.0 compatible

---
+++ agenda/agendaEventSelection.server/src/main/java/org/universAAL/agendaEventSelectionTool/server/Activator.java
@@ -0,0 +1,31 @@
+package org.universAAL.agendaEventSelectionTool.server;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+
+public class Activator implements BundleActivator {
+	/**
+	 * {@link ModuleContext}
+	 */
+	private static ModuleContext mcontext;
+
+	/**
+	 * 
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 *      )
+	 */
+	public void start(BundleContext context) throws Exception {
+		BundleContext[] bc = { context };
+		mcontext = uAALBundleContainer.THE_CONTAINER.registerModule(bc);
+		new EventProvider(mcontext);
+	}
+
+	/**
+	 * 
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+	}
+}
---
+++ agenda/agendaEventSelection.server/src/main/java/org/universAAL/agendaEventSelectionTool/server/Messages.java
@@ -0,0 +1,23 @@
+package org.universAAL.agendaEventSelectionTool.server;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Messages {
+	private static final String BUNDLE_NAME = "org.universAAL.agendaEventSelectionTool.server.messages_"+Locale.getDefault().getLanguage().toLowerCase(); //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);//, Locale.ENGLISH);
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
