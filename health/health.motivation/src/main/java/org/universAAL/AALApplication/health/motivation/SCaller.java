7a57766de5bf185684da77c79215780910cd8770
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-01-25 17:37:36 +0000

    Merged FROM SANDBOX

--- health/health.motivation/src/main/java/org/universAAL/AALApplication/health/motivation/SCaller.java
+++ health/health.motivation/src/main/java/org/universAAL/AALApplication/health/motivation/SCaller.java
@@ -1,12 +1,12 @@
 package org.universAAL.AALApplication.health.motivation;
 
-import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.container.ModuleContext;
 import org.universAAL.middleware.service.ServiceCaller;
 import org.universAAL.middleware.service.ServiceResponse;
 
 public class SCaller extends ServiceCaller{
 
-	protected SCaller(BundleContext context) {
+	protected SCaller(ModuleContext context) {
 		super(context);
 		// TODO Auto-generated constructor stub
 	}
