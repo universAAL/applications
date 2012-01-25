5d64e290bd5f0083faeaf631704e76f9fff9adac
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-01-25 16:40:40 +0000

    updated runner

--- personal_safety/run/runner/run.bat
+++ personal_safety/run/runner/run.bat
@@ -1,1 +1,1 @@
-java -Dfelix.config.properties=file:felix/config.ini -Dfelix.system.properties=file:felix/system.properties -jar bundles/org.apache.felix.main_1.4.1.jar
+java -Dfelix.config.properties=file:felix/config.ini -Dfelix.system.properties=file:felix/system.properties -jar bundles/org.apache.felix.main_4.0.2.jar
