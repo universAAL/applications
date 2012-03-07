c4417e7a23865788695c2d8fa49b194c1efbcb71
mr Andrej Grguric <andrej.grguric@ericsson.com>
2012-03-07 17:09:38 +0000

    agenda poms grooming

--- agenda/agenda.server/src/main/java/org/universAAL/agenda/server/unit_impl/CheckDatabaseTask.java
+++ agenda/agenda.server/src/main/java/org/universAAL/agenda/server/unit_impl/CheckDatabaseTask.java
@@ -1,6 +1,5 @@
 package org.universAAL.agenda.server.unit_impl;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TimerTask;
@@ -9,26 +8,22 @@ import org.universAAL.agenda.server.database.AgendaDBInterface;
 import org.universAAL.agenda.server.database.Scheduler;
 
 public abstract class CheckDatabaseTask extends TimerTask {
-	public final static int DB_CHECK_TIME_PERIOD = 65 * 60 * 1000; // 1 hour
-																	// and 5
-																	// minutes
-																	// (value
-																	// in
-																	// seconds)
-	public final static int DB_CHECK_INTERVAL = 60*30*1000; // 1 hour (value
-															// in
-															// milliseconds)
-	protected AgendaDBInterface dbServer;
-	protected Scheduler scheduler;
-	protected List list;
+    // 1 hour and 5 minutes (value in miliseconds)
+    public final static int DB_CHECK_TIME_PERIOD = 65 * 60 * 1000;
 
-	public CheckDatabaseTask(AgendaDBInterface dbServer, Scheduler scheduler) {
-		this.dbServer = dbServer;
-		this.scheduler = scheduler;
-		list = new ArrayList();
-	}
+    // 0.5 hour (value in milliseconds)
+    public final static int DB_CHECK_INTERVAL = 60 * 30 * 1000;
+    protected AgendaDBInterface dbServer;
+    protected Scheduler scheduler;
+    protected List list;
 
-	public abstract void run(); 
+    public CheckDatabaseTask(AgendaDBInterface dbServer, Scheduler scheduler) {
+	this.dbServer = dbServer;
+	this.scheduler = scheduler;
+	list = new ArrayList();
+    }
 
-	public abstract void populateScheduler(); 
+    public abstract void run();
+
+    public abstract void populateScheduler();
 }
