8f5b950c03bf0561a5c4289bbb524931a8fec035
mr Andrej Grguric <andrej.grguric@ericsson.com>
2012-03-07 17:09:38 +0000

    agenda poms grooming

--- agenda/agenda.server/src/main/java/org/universAAL/agenda/server/unit_impl/AgendaStateListener.java
+++ agenda/agenda.server/src/main/java/org/universAAL/agenda/server/unit_impl/AgendaStateListener.java
@@ -1,20 +1,22 @@
-/**
- * 
- */
 package org.universAAL.agenda.server.unit_impl;
 
-import org.universAAL.agenda.ont.*;
-
+import org.universAAL.agenda.ont.Calendar;
+import org.universAAL.agenda.ont.Event;
 
 /**
  * @author kagnantis
- *
+ * 
  */
-public interface AgendaStateListener {	
-	public void eventDeleted(Calendar calendar, int eventID);
-	public void eventAdded(Calendar calendar, Event e);
-	public void eventUpdated(Calendar calendar, Event e);
-	public void reminderTime(Event e);
-	public void startEventTime(Event e);
-	public void endEventTime(Event e);
+public interface AgendaStateListener {
+    public void eventDeleted(Calendar calendar, int eventID);
+
+    public void eventAdded(Calendar calendar, Event e);
+
+    public void eventUpdated(Calendar calendar, Event e);
+
+    public void reminderTime(Event e);
+
+    public void startEventTime(Event e);
+
+    public void endEventTime(Event e);
 }
