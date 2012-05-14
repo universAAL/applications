9693c1722e2bf9d6a967ad364c1fd051078361a2
Mr. Venelin Arnaudov <v.arnaudov@prosyst.com>
2012-05-14 07:07:16 +0000

    Renamed medication_manager.service folder to medication.manager.service

---
+++ medication/medication.manager.service/src/main/java/org/universAAL/AALapplication/medication_manager/impl/PrecautionProvider.java
@@ -0,0 +1,79 @@
+package org.universAAL.AALapplication.medication_manager.impl;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.service.CallStatus;
+import org.universAAL.middleware.service.ServiceCall;
+import org.universAAL.middleware.service.ServiceCallee;
+import org.universAAL.middleware.service.ServiceResponse;
+import org.universAAL.middleware.service.owls.process.ProcessOutput;
+import org.universAAL.ontology.medMgr.Precaution;
+
+/**
+ * @author George Fournadjiev
+ */
+public final class PrecautionProvider extends ServiceCallee {
+
+  private final MyPrecautionDatabase myPrecaution;
+
+  // this is just to prepare a standard error message for later use
+  private static final ServiceResponse invalidInput = new ServiceResponse(
+      CallStatus.serviceSpecificFailure);
+
+  static {
+    invalidInput.addOutput(new ProcessOutput(
+        ServiceResponse.PROP_SERVICE_SPECIFIC_ERROR, "Invalid input!"));
+  }
+
+  public PrecautionProvider(ModuleContext context) {
+    super(context, ProviderPrecautionService.profiles);
+
+    myPrecaution = new MyPrecautionDatabase();
+  }
+
+  public void communicationChannelBroken() {
+    //Not implemented yet
+  }
+
+  public ServiceResponse handleCall(ServiceCall call) {
+    String processURI = call.getProcessURI();
+
+    Log.info("Received call %s", getClass(), processURI);
+
+    Resource involvedUser = call.getInvolvedUser();
+
+    Log.info("involvedUser %s", getClass(), involvedUser);
+
+    if (involvedUser == null) {
+      return invalidInput;
+    }
+
+    if (processURI.startsWith(ProviderPrecautionService.SERVICE_GET_PRECAUTION)) {
+      return getSuccessfulServiceResponse(involvedUser);
+    }
+
+    return invalidInput;
+  }
+
+  private ServiceResponse getSuccessfulServiceResponse(Resource involvedUser) {
+    String userId = involvedUser.getURI();
+    Log.info("Successful Service Response for the user %s", getClass(), userId);
+    return getPrecaution(userId);
+  }
+
+  private ServiceResponse getPrecaution(String userId) {
+    ServiceResponse response = new ServiceResponse(CallStatus.succeeded);
+
+    Precaution precaution = MyPrecautionDatabase.getPrecaution(userId);
+
+    Log.info("Found the following precaution: %s", getClass(), precaution);
+
+    if (precaution == null) {
+      return invalidInput;
+    }
+
+    response.addOutput(new ProcessOutput(ProviderPrecautionService.OUTPUT_PRECAUTION, precaution));
+
+    return response;
+  }
+}
