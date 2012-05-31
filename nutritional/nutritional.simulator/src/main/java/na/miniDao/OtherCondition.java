442fe2d3729bed72b0ead1da513a3784783f5607
Hector Galan <hecgamar@itaca.upv.es>
2012-05-31 14:10:40 +0000

    tryal 1

---
+++ nutritional/nutritional.simulator/src/main/java/na/miniDao/OtherCondition.java
@@ -0,0 +1,357 @@
+/**
+ * OtherCondition.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package na.miniDao;
+
+public class OtherCondition  implements java.io.Serializable {
+    private boolean high_temperature;
+
+    private boolean consumes_alcohol;
+
+    private boolean smokes;
+
+    private boolean has_chronic_disease;
+
+    private boolean has_cholesterol;
+
+    private boolean has_cardiovascular;
+
+    private boolean has_diabetes;
+
+    private boolean has_digestive_problem;
+
+    private boolean has_overweight;
+
+    private boolean has_urine_sugar;
+
+    private boolean has_respiratory_problem;
+
+    private boolean has_highFatigue;
+
+    public OtherCondition() {
+    }
+
+    public OtherCondition(
+           boolean high_temperature,
+           boolean consumes_alcohol,
+           boolean smokes,
+           boolean has_chronic_disease,
+           boolean has_cholesterol,
+           boolean has_cardiovascular,
+           boolean has_diabetes,
+           boolean has_digestive_problem,
+           boolean has_overweight,
+           boolean has_urine_sugar,
+           boolean has_respiratory_problem,
+           boolean has_highFatigue) {
+           this.high_temperature = high_temperature;
+           this.consumes_alcohol = consumes_alcohol;
+           this.smokes = smokes;
+           this.has_chronic_disease = has_chronic_disease;
+           this.has_cholesterol = has_cholesterol;
+           this.has_cardiovascular = has_cardiovascular;
+           this.has_diabetes = has_diabetes;
+           this.has_digestive_problem = has_digestive_problem;
+           this.has_overweight = has_overweight;
+           this.has_urine_sugar = has_urine_sugar;
+           this.has_respiratory_problem = has_respiratory_problem;
+           this.has_highFatigue = has_highFatigue;
+    }
+
+
+    /**
+     * Gets the high_temperature value for this OtherCondition.
+     * 
+     * @return high_temperature
+     */
+    public boolean isHigh_temperature() {
+        return high_temperature;
+    }
+
+
+    /**
+     * Sets the high_temperature value for this OtherCondition.
+     * 
+     * @param high_temperature
+     */
+    public void setHigh_temperature(boolean high_temperature) {
+        this.high_temperature = high_temperature;
+    }
+
+
+    /**
+     * Gets the consumes_alcohol value for this OtherCondition.
+     * 
+     * @return consumes_alcohol
+     */
+    public boolean isConsumes_alcohol() {
+        return consumes_alcohol;
+    }
+
+
+    /**
+     * Sets the consumes_alcohol value for this OtherCondition.
+     * 
+     * @param consumes_alcohol
+     */
+    public void setConsumes_alcohol(boolean consumes_alcohol) {
+        this.consumes_alcohol = consumes_alcohol;
+    }
+
+
+    /**
+     * Gets the smokes value for this OtherCondition.
+     * 
+     * @return smokes
+     */
+    public boolean isSmokes() {
+        return smokes;
+    }
+
+
+    /**
+     * Sets the smokes value for this OtherCondition.
+     * 
+     * @param smokes
+     */
+    public void setSmokes(boolean smokes) {
+        this.smokes = smokes;
+    }
+
+
+    /**
+     * Gets the has_chronic_disease value for this OtherCondition.
+     * 
+     * @return has_chronic_disease
+     */
+    public boolean isHas_chronic_disease() {
+        return has_chronic_disease;
+    }
+
+
+    /**
+     * Sets the has_chronic_disease value for this OtherCondition.
+     * 
+     * @param has_chronic_disease
+     */
+    public void setHas_chronic_disease(boolean has_chronic_disease) {
+        this.has_chronic_disease = has_chronic_disease;
+    }
+
+
+    /**
+     * Gets the has_cholesterol value for this OtherCondition.
+     * 
+     * @return has_cholesterol
+     */
+    public boolean isHas_cholesterol() {
+        return has_cholesterol;
+    }
+
+
+    /**
+     * Sets the has_cholesterol value for this OtherCondition.
+     * 
+     * @param has_cholesterol
+     */
+    public void setHas_cholesterol(boolean has_cholesterol) {
+        this.has_cholesterol = has_cholesterol;
+    }
+
+
+    /**
+     * Gets the has_cardiovascular value for this OtherCondition.
+     * 
+     * @return has_cardiovascular
+     */
+    public boolean isHas_cardiovascular() {
+        return has_cardiovascular;
+    }
+
+
+    /**
+     * Sets the has_cardiovascular value for this OtherCondition.
+     * 
+     * @param has_cardiovascular
+     */
+    public void setHas_cardiovascular(boolean has_cardiovascular) {
+        this.has_cardiovascular = has_cardiovascular;
+    }
+
+
+    /**
+     * Gets the has_diabetes value for this OtherCondition.
+     * 
+     * @return has_diabetes
+     */
+    public boolean isHas_diabetes() {
+        return has_diabetes;
+    }
+
+
+    /**
+     * Sets the has_diabetes value for this OtherCondition.
+     * 
+     * @param has_diabetes
+     */
+    public void setHas_diabetes(boolean has_diabetes) {
+        this.has_diabetes = has_diabetes;
+    }
+
+
+    /**
+     * Gets the has_digestive_problem value for this OtherCondition.
+     * 
+     * @return has_digestive_problem
+     */
+    public boolean isHas_digestive_problem() {
+        return has_digestive_problem;
+    }
+
+
+    /**
+     * Sets the has_digestive_problem value for this OtherCondition.
+     * 
+     * @param has_digestive_problem
+     */
+    public void setHas_digestive_problem(boolean has_digestive_problem) {
+        this.has_digestive_problem = has_digestive_problem;
+    }
+
+
+    /**
+     * Gets the has_overweight value for this OtherCondition.
+     * 
+     * @return has_overweight
+     */
+    public boolean isHas_overweight() {
+        return has_overweight;
+    }
+
+
+    /**
+     * Sets the has_overweight value for this OtherCondition.
+     * 
+     * @param has_overweight
+     */
+    public void setHas_overweight(boolean has_overweight) {
+        this.has_overweight = has_overweight;
+    }
+
+
+    /**
+     * Gets the has_urine_sugar value for this OtherCondition.
+     * 
+     * @return has_urine_sugar
+     */
+    public boolean isHas_urine_sugar() {
+        return has_urine_sugar;
+    }
+
+
+    /**
+     * Sets the has_urine_sugar value for this OtherCondition.
+     * 
+     * @param has_urine_sugar
+     */
+    public void setHas_urine_sugar(boolean has_urine_sugar) {
+        this.has_urine_sugar = has_urine_sugar;
+    }
+
+
+    /**
+     * Gets the has_respiratory_problem value for this OtherCondition.
+     * 
+     * @return has_respiratory_problem
+     */
+    public boolean isHas_respiratory_problem() {
+        return has_respiratory_problem;
+    }
+
+
+    /**
+     * Sets the has_respiratory_problem value for this OtherCondition.
+     * 
+     * @param has_respiratory_problem
+     */
+    public void setHas_respiratory_problem(boolean has_respiratory_problem) {
+        this.has_respiratory_problem = has_respiratory_problem;
+    }
+
+
+    /**
+     * Gets the has_highFatigue value for this OtherCondition.
+     * 
+     * @return has_highFatigue
+     */
+    public boolean isHas_highFatigue() {
+        return has_highFatigue;
+    }
+
+
+    /**
+     * Sets the has_highFatigue value for this OtherCondition.
+     * 
+     * @param has_highFatigue
+     */
+    public void setHas_highFatigue(boolean has_highFatigue) {
+        this.has_highFatigue = has_highFatigue;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof OtherCondition)) return false;
+        OtherCondition other = (OtherCondition) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            this.high_temperature == other.isHigh_temperature() &&
+            this.consumes_alcohol == other.isConsumes_alcohol() &&
+            this.smokes == other.isSmokes() &&
+            this.has_chronic_disease == other.isHas_chronic_disease() &&
+            this.has_cholesterol == other.isHas_cholesterol() &&
+            this.has_cardiovascular == other.isHas_cardiovascular() &&
+            this.has_diabetes == other.isHas_diabetes() &&
+            this.has_digestive_problem == other.isHas_digestive_problem() &&
+            this.has_overweight == other.isHas_overweight() &&
+            this.has_urine_sugar == other.isHas_urine_sugar() &&
+            this.has_respiratory_problem == other.isHas_respiratory_problem() &&
+            this.has_highFatigue == other.isHas_highFatigue();
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        _hashCode += (isHigh_temperature() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isConsumes_alcohol() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isSmokes() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isHas_chronic_disease() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isHas_cholesterol() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isHas_cardiovascular() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isHas_diabetes() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isHas_digestive_problem() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isHas_overweight() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isHas_urine_sugar() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isHas_respiratory_problem() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        _hashCode += (isHas_highFatigue() ? Boolean.TRUE : Boolean.FALSE).hashCode();
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+
+}
