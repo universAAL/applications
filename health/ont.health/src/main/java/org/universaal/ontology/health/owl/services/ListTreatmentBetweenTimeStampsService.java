b0aaa3b78e74f40efab2c0c7ac4d6032b6bf624f
Mr Dario Salvi <dsalvi@lst.tfo.upm.es>
2012-07-25 10:33:01 +0000

    moved to CORRECT PLACE!!!!! @Marina: look at your config!!

---
+++ health/ont.health/src/main/java/org/universaal/ontology/health/owl/services/ListTreatmentBetweenTimeStampsService.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright 2012 UPM, http://www.upm.es 
+ * Universidad Politécnica de Madrid
+ * 
+ *	OCO Source Materials
+ *	� Copyright IBM Corp. 2011
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.universaal.ontology.health.owl.services;
+
+import org.universAAL.middleware.rdf.TypeMapper;
+import org.universAAL.ontology.profile.User;
+import org.universaal.ontology.health.owl.HealthOntology;
+import org.universaal.ontology.health.owl.Treatment;
+
+/**
+ * @author amedrano
+ * @author roni
+ *
+ * @navassoc - "lists" * Treatment
+ */
+public class ListTreatmentBetweenTimeStampsService extends ListTreatmentService {
+
+	//NAMESPACE & PROPERTIES
+	public static final String MY_URI = HealthOntology.NAMESPACE
+			+ "ListTreatmentBetweenTimeStampsService";
+	
+	public static final String PROP_TIMESTAMP_FROM =  HealthOntology.NAMESPACE
+		+ "listsTreatmentsTimestampFrom";
+	public static final String PROP_TIMESTAMP_TO =  HealthOntology.NAMESPACE
+		+ "listsTreatmentsTimestampTo";
+
+	public static final String INPUT_TIMESTAMP_FROM = HealthOntology.NAMESPACE + "timestampFrom";
+
+	public static final String INPUT_TIMESTAMP_TO   = HealthOntology.NAMESPACE + "timestampTo";
+
+	//CONSTRUCTOR	
+	public ListTreatmentBetweenTimeStampsService() {
+		super();
+		buildProfile();
+	}		
+	
+	public ListTreatmentBetweenTimeStampsService(String instanceURI) {
+		super(instanceURI);
+		buildProfile();
+	}
+
+	private void buildProfile() {
+		addFilteringInput(INPUT_USER, User.MY_URI, 1, 1, 
+    			new String[] { PROP_ASSISTED_USER });
+    	addFilteringInput(INPUT_TIMESTAMP_FROM, TypeMapper.getDatatypeURI(Long.class), 1, 1, 
+    			new String[] { PROP_TIMESTAMP_FROM });
+    	addFilteringInput(INPUT_TIMESTAMP_TO, TypeMapper.getDatatypeURI(Long.class), 1, 1, 
+    			new String[] { PROP_TIMESTAMP_TO });
+    	addOutput(OUTPUT_TREATMENTS, Treatment.MY_URI, 0, -1, 
+    			new String[] { PROP_LISTS_TREATMENTS });
+	}
+	
+	public String getClassURI() {
+		return MY_URI;
+	}
+
+	public int getPropSerializationType(String propURI) {
+		return PROP_SERIALIZATION_FULL;
+	}
+
+	public boolean isWellFormed() {
+		return true;
+	}
+}
