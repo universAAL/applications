eb195c368cb8804370d2493a3b90b2e9b79bd4f9
Mss. Marina de la Fuente <mdelafuente@lst.tfo.upm.es>
2012-07-25 15:16:00 +0000

    motivational interface methods implemented

--- health/health.motivation/src/main/java/org/universAAL/AALApplication/health/motivation/Motivation.java
+++ health/health.motivation/src/main/java/org/universAAL/AALApplication/health/motivation/Motivation.java
@@ -2,6 +2,8 @@ package org.universAAL.AALApplication.health.motivation;
 
 import java.util.Locale;
 
+import org.universAAL.ontology.profile.User;
+import org.universaal.ontology.health.owl.HealthProfile;
 import org.universaal.ontology.health.owl.MotivationalStatusType;
 import org.universaal.ontology.owl.MotivationalMessage;
 import org.universaal.ontology.owl.MotivationalMessageClassification;
@@ -44,6 +46,21 @@ public class Motivation implements MotivationInterface{
 	}
 
 	public String setMessageVariablesRoute(String variablesDatabaseRoute) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User getAssistedPerson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User getCaregiver(User assistedPerson) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public HealthProfile getHealthProfile(User u) {
 		// TODO Auto-generated method stub
 		return null;
 	}
