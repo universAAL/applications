dbe59d079d331491c04d893c83ef449c0b51b2ea
Ioannis Moulos <moulos@certh.gr>
2012-09-19 11:34:34 +0000

    corrected install.bat

--- health/PhysicalActivityMonitorModule/install.bat
+++ health/PhysicalActivityMonitorModule/install.bat
@@ -1,3 +1,3 @@
-call mvn install:install-file -Dfile=./test-jars/physical_activity_monitor/bluecove-2.1.0.jar -DgroupId=org.universAAL.AALapplicationUtils.bluecove -DartifactId=bluecove -Dversion=2.1.0 -Dpackaging=jar
-call mvn install:install-file -Dfile=./test-jars/physical_activity_monitor/zephyrutils-1.0.jar -DgroupId=org.universAAL.AALapplicationUtils.zephyrutils -DartifactId=zephyrutils -Dversion=1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=./external-jars/physical_activity_monitor/bluecove-2.1.0.jar -DgroupId=org.universAAL.AALapplicationUtils.bluecove -DartifactId=bluecove -Dversion=2.1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=./external-jars/physical_activity_monitor/zephyrutils-1.0.jar -DgroupId=org.universAAL.AALapplicationUtils.zephyrutils -DartifactId=zephyrutils -Dversion=1.0 -Dpackaging=jar
 
