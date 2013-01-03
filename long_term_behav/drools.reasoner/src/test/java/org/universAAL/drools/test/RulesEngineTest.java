a7a53f3373b1bba7ed1ffaca45c7bc5e717bb77d
Miguel A. Llorente <mllorente@tsbtecnologias.es>
2013-01-03 16:27:10 +0000

    Enriched POMs and some bugs fixed.

--- long_term_behav/drools.reasoner/src/test/java/org/universAAL/drools/test/RulesEngineTest.java
+++ long_term_behav/drools.reasoner/src/test/java/org/universAAL/drools/test/RulesEngineTest.java
@@ -1,28 +1,26 @@
 package org.universAAL.drools.test;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertNotNull;
 import junit.framework.TestCase;
 
-import org.junit.Test;
 import org.universAAL.drools.engine.RulesEngine;
 
-public class RulesEngineTest {
-	@Test
-	void testCreation() {
-//		String lepath = System.getProperty("java.class.path");
-//		System
-//				.setProperty(
-//						"java.class.path",
-//						lepath.substring(0, lepath.length() - 1)
-//								+ ";C:\\Desarrollo\\drools\\runtime5\\org.eclipse.jdt.core_3.5.2.v_981_R35x.jar");
-//		String[] paths = System.getProperty("java.class.path").split(";");
-//
-//		for (String string : paths) {
-//			System.out.println(string);
-//		}
+public class RulesEngineTest extends TestCase {
+
+	public void testCreation() {
+		// String lepath = System.getProperty("java.class.path");
+		// System
+		// .setProperty(
+		// "java.class.path",
+		// lepath.substring(0, lepath.length() - 1)
+		// +
+		// ";C:\\Desarrollo\\drools\\runtime5\\org.eclipse.jdt.core_3.5.2.v_981_R35x.jar");
+		// String[] paths = System.getProperty("java.class.path").split(";");
+		//
+		// for (String string : paths) {
+		// System.out.println(string);
+		// }
 		assertNotNull(RulesEngine.getInstance());
-		//RulesEngine re = RulesEngine.createRulesEngine(null);
+		// RulesEngine re = RulesEngine.createRulesEngine(null);
 	}
 
 }
