84ff8f96ee0bcc8fa06bfa97839b5ee3f873d80f
mr Andrej Grguric <andrej.grguric@ericsson.com>
2013-02-05 18:55:42 +0000

    lang now read from VM parameters, if there is none en is used, not from Locale (which cannot be changed)

--- agenda/agenda.gui/src/main/java/org/universAAL/agenda/gui/util/Messages.java
+++ agenda/agenda.gui/src/main/java/org/universAAL/agenda/gui/util/Messages.java
@@ -1,15 +1,15 @@
 package org.universAAL.agenda.gui.util;
 
-import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
 	//private static final String BUNDLE_NAME = "org.universAAL.agenda.gui.util.messages_" + Locale.getDefault().getLanguage().toLowerCase(); //$NON-NLS-1$
 
-    private static final String BUNDLE_NAME = "org.universAAL.agenda.gui.util.messages";
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(BUNDLE_NAME, Locale.getDefault()); 
+    private static final String BUNDLE_NAME = "org.universAAL.agenda.gui.util.messages_"
+	    + System.getProperty("user.language", "en");
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+	    .getBundle(BUNDLE_NAME);
 
 	private Messages() {
 	}
