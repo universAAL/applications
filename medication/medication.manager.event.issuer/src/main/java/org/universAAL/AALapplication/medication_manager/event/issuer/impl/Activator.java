e0e621f1d107fc97272ef9a3c3bccb19c2214033
Mr. Venelin Arnaudov <v.arnaudov@prosyst.com>
2013-05-21 11:15:13 +0000

    Started work on the event issuer medication  module

---
+++ medication/medication.manager.event.issuer/src/main/java/org/universAAL/AALapplication/medication_manager/event/issuer/impl/Activator.java
@@ -0,0 +1,43 @@
+package org.universAAL.AALapplication.medication_manager.event.issuer.impl;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+
+/**
+ * @author George Fournadjiev
+ */
+public final class Activator implements BundleActivator {
+
+
+  public static ModuleContext mc;
+
+  public void start(final BundleContext context) throws Exception {
+    mc = uAALBundleContainer.THE_CONTAINER.registerModule(new Object[]{context});
+
+
+  }
+
+  public void stop(BundleContext context) throws Exception {
+
+  }
+
+  public static void validateParameter(int parameter, String parameterName) {
+
+    if (parameter <= 0) {
+      throw new MedicationManagerEventIssuerException("The parameter : " +
+          parameterName + " must be positive number");
+    }
+
+  }
+
+  public static void validateParameter(Object parameter, String parameterName) {
+
+    if (parameter == null) {
+      throw new MedicationManagerEventIssuerException("The parameter : " + parameterName + " cannot be null");
+    }
+
+  }
+
+}
