ff96095afc68df96fbfe91366d50afc9a6ffce8b
Mrs Gema Ibañez <geibsan@itaca.upv.es>
2013-05-28 16:35:19 +0000

    it has been removed because it is maintained in old_services project, and doesnt make sense to keep it in both places.

--- help_when_out/helpwhenoutside/trunk/src/main/java/org/persona/service/helpwhenoutside/HwoNamespace.java
+++
@@ -1,9 +0,0 @@
-package org.persona.service.helpwhenoutside;
-
-
-public interface HwoNamespace {
-
-  static final String NAMESPACE = "http://ontology.universAAL.org/hwoutside.owl#"; //Ontology's Head Name
-
-}	
-
--- help_when_out/helpwhenoutside/trunk/src/main/java/org/persona/service/helpwhenoutside/common/AddressLocation.java
+++
@@ -1,9 +0,0 @@
-package org.persona.service.helpwhenoutside.common;
-
-public class AddressLocation {
-	public double latitude;
-	public double longitude;
-	public String address;
-	public long time;
-	public long id;
-} 
