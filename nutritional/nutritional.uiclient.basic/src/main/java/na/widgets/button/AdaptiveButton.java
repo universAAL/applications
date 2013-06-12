30657d0942ed0d4eaf75c6997c1a8482d7e3e42f
Mrs Gema Ibañez <geibsan@itaca.upv.es>
2013-06-12 15:10:35 +0000

    Removed deprecated projects

--- nutritional/nutritional.uiclient.basic/src/main/java/na/widgets/button/AdaptiveButton.java
+++
@@ -1,7 +0,0 @@
-package na.widgets.button;
-
-import javax.swing.JButton;
-
-public class AdaptiveButton extends JButton{
-
-}
--- nutritional/nutritional.uiclient.basic/src/main/java/na/widgets/textfield/AdaptiveTextField.java
+++
@@ -1,7 +0,0 @@
-package na.widgets.textfield;
-
-import javax.swing.JTextField;
-
-public class AdaptiveTextField extends JTextField {
-
-}
