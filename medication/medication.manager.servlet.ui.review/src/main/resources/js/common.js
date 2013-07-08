c6ed7f981896318012f4149d9b024548887f5224
Mr. Venelin Arnaudov <v.arnaudov@prosyst.com>
2013-07-08 12:01:15 +0000

    removed things not related to the review web application

--- medication/medication.manager.servlet.ui.review/src/main/resources/js/common.js
+++ medication/medication.manager.servlet.ui.review/src/main/resources/js/common.js
@@ -5,8 +5,6 @@ isStatic = false;
 var LOGIN_SERVLET_ALIAS = "/review/login",
         SELECT_USER_SERVLET_ALIAS = "/review/select_user",
         HANDLE_SELECT_USER_SERVLET_ALIAS = "/review/handle_user",
-        SELECT_MEDICINE_SERVLET_ALIAS = "/review/select_med",
-        HANDLE_MEDICINE_SERVLET_ALIAS = "/review/handle_med",
         HANDLE_SUCCESS_SERVLET_ALIAS = "/review/handle_success",
         CANCEL = "?cancel=true";
         BACK = "?back=true";
@@ -18,20 +16,7 @@ $(function () {
         e.preventDefault();
         document.location.href = LOGIN_SERVLET_ALIAS + CANCEL;
     });
-    $('form[name="select_med"]').attr("action", HANDLE_MEDICINE_SERVLET_ALIAS);
-    $('form[name="select_med"] button[name="back"]').click(function (e) {
-        e.preventDefault();
-        document.location.href = HANDLE_MEDICINE_SERVLET_ALIAS + BACK;
-    });
-    $('form[name="success"]').attr("action", HANDLE_SUCCESS_SERVLET_ALIAS);
-    $('form[name="success"] button[name="back"]').click(function (e) {
-        e.preventDefault();
-        document.location.href = HANDLE_SUCCESS_SERVLET_ALIAS + BACK;
-    });
-    $('form[name="success"] button[name="logout"]').click(function (e) {
-        e.preventDefault();
-        document.location.href = HANDLE_SUCCESS_SERVLET_ALIAS + CANCEL;
-    });
+
 
 
 });
