57fcefb8edea4acfe43d69e4620f60aec8fd976f
Mr. Venelin Arnaudov <v.arnaudov@prosyst.com>
2013-07-08 14:31:49 +0000

    added color (red, green, black) depending on the intake status

--- medication/medication.manager.servlet.ui.review/src/main/resources/js/intakes.js
+++ medication/medication.manager.servlet.ui.review/src/main/resources/js/intakes.js
@@ -4,7 +4,7 @@ $(function () {
 
     var tableSelector = 'table';
     var templSelector = tableSelector + ' tr.templ';
-    if ($.isEmptyObject(intakes)){
+    if ($.isEmptyObject(intakes)) {
         $(templSelector).remove();
         $("input:submit").hide();
         return;
@@ -15,9 +15,14 @@ $(function () {
     $.each(intakes, function (id, intake) {
         var tr = $trTempl.clone(true);
         tr.find('td:eq(0)').html(intake["date"])
-            .next('td').html(intake["time"])
-            .next('td').html(intake["medication"])
-            .next('td').html(intake["status"]);
+                .next('td').html(intake["time"])
+                .next('td').html(intake["medication"])
+                .next('td').html(intake["status"]);
+        if (intake["status"] == "missed") {
+            tr.addClass("red")
+        } else if (intake["status"] == "taken") {
+            tr.addClass("green")
+        }
 
         $(tableSelector).append(tr);
 
