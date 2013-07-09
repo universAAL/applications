51669f999e409a2fb09d98f15641a8c531181026
Mr. Venelin Arnaudov <v.arnaudov@prosyst.com>
2013-07-09 12:13:59 +0000

    added display servlet for intakes.html

--- medication/medication.manager.servlet.ui.review/src/main/java/org/universAAL/AALapplication/medication_manager/servlet/ui/review/impl/servlets/DisplayIntakesHtmlWriterServlet.java
+++ medication/medication.manager.servlet.ui.review/src/main/java/org/universAAL/AALapplication/medication_manager/servlet/ui/review/impl/servlets/DisplayIntakesHtmlWriterServlet.java
@@ -1,38 +1,32 @@
 package org.universAAL.AALapplication.medication_manager.servlet.ui.review.impl.servlets;
 
 import org.universAAL.AALapplication.medication_manager.persistence.layer.PersistentService;
-import org.universAAL.AALapplication.medication_manager.persistence.layer.dao.PatientLinksDao;
 import org.universAAL.AALapplication.medication_manager.persistence.layer.entities.Person;
-import org.universAAL.AALapplication.medication_manager.servlet.ui.review.impl.Log;
-import org.universAAL.AALapplication.medication_manager.servlet.ui.review.impl.MedicationManagerReviewException;
-import org.universAAL.AALapplication.medication_manager.servlet.ui.review.impl.servlets.forms.UserSelectScriptForm;
 import org.universAAL.AALapplication.medication_manager.servlet.ui.base.export.helpers.Session;
 import org.universAAL.AALapplication.medication_manager.servlet.ui.base.export.helpers.SessionTracking;
+import org.universAAL.AALapplication.medication_manager.servlet.ui.base.export.parser.script.forms.DisplayIntakesScriptForm;
 import org.universAAL.AALapplication.medication_manager.servlet.ui.base.export.parser.script.forms.ScriptForm;
+import org.universAAL.AALapplication.medication_manager.servlet.ui.review.impl.Log;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
+import static org.universAAL.AALapplication.medication_manager.servlet.ui.base.export.helpers.ServletUtil.*;
 import static org.universAAL.AALapplication.medication_manager.servlet.ui.review.impl.Activator.*;
 import static org.universAAL.AALapplication.medication_manager.servlet.ui.review.impl.Util.*;
-import static org.universAAL.AALapplication.medication_manager.servlet.ui.base.export.helpers.ServletUtil.*;
 
 
 /**
  * @author George Fournadjiev
  */
-public final class SelectUserHtmlWriterServlet extends BaseHtmlWriterServlet {
+public final class DisplayIntakesHtmlWriterServlet extends BaseHtmlWriterServlet {
 
   private final Object lock = new Object();
   private DisplayLoginHtmlWriterServlet displayServlet;
 
-  private static final String USER_HTML_FILE_NAME = "user.html";
-
-  public SelectUserHtmlWriterServlet(SessionTracking sessionTracking) {
-    super(USER_HTML_FILE_NAME, sessionTracking);
+  public DisplayIntakesHtmlWriterServlet(SessionTracking sessionTracking) {
+    super(INTAKES_FILE_NAME, sessionTracking);
   }
 
   public void setDisplayServlet(DisplayLoginHtmlWriterServlet displayServlet) {
@@ -55,14 +49,10 @@ public final class SelectUserHtmlWriterServlet extends BaseHtmlWriterServlet {
           return;
         }
 
-        String cancel = req.getParameter(CANCEL);
-        if (cancel != null && TRUE.equalsIgnoreCase(cancel)) {
-          debugSessions(session.getId(), "cancel the servlet doGet/doPost method", getClass());
-          displayServlet.doGet(req, resp);
-          return;
-        }
+        debugSessions(session.getId(), "End of the servlet doGet/doPost method", getClass());
 
-        handleResponse(req, resp, caregiver, session);
+        handleResponse(req, resp, caregiver);
+
       } catch (Exception e) {
         Log.error(e.fillInStackTrace(), "Unexpected Error occurred", getClass());
         sendErrorResponse(req, resp, e);
@@ -71,24 +61,19 @@ public final class SelectUserHtmlWriterServlet extends BaseHtmlWriterServlet {
   }
 
   @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     doGet(req, resp);
   }
 
 
-  private void handleResponse(HttpServletRequest req, HttpServletResponse resp,
-                              Person caregiver, Session session) throws IOException {
-
-    PersistentService persistentService = getPersistentService();
-    PatientLinksDao patientLinksDao = persistentService.getPatientLinksDao();
-    List<Person> patients =patientLinksDao.findCaregiverPatients(caregiver);
-    if (patients != null && !patients.isEmpty()) {
-      ScriptForm scriptForm = new UserSelectScriptForm(patients);
+  private void handleResponse(HttpServletRequest req, HttpServletResponse resp, Person caregiver) throws IOException {
+    try {
+      PersistentService persistentService = getPersistentService();
+      ScriptForm scriptForm = new DisplayIntakesScriptForm(persistentService);
       sendResponse(req, resp, scriptForm);
-    } else {
-      throw new MedicationManagerReviewException("Missing patients for the following caregiver: " + caregiver.getName());
+    } catch (Exception e) {
+      sendErrorResponse(req, resp, e);
     }
-
   }
 
 
