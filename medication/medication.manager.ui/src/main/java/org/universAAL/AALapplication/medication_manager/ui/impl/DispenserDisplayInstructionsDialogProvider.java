447687979520277ad60f4cad275efe12f73229be
Mr. Venelin Arnaudov <v.arnaudov@prosyst.com>
2013-07-26 15:03:10 +0000

    Added Apache licences in the top of the every java source file

--- medication/medication.manager.ui/src/main/java/org/universAAL/AALapplication/medication_manager/ui/impl/DispenserDisplayInstructionsDialogProvider.java
+++ medication/medication.manager.ui/src/main/java/org/universAAL/AALapplication/medication_manager/ui/impl/DispenserDisplayInstructionsDialogProvider.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2012 , http://www.prosyst.com - ProSyst Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package org.universAAL.AALapplication.medication_manager.ui.impl;
 
 import org.universAAL.AALapplication.medication_manager.ui.DispenserDisplayInstructionsDialog;
@@ -10,24 +26,6 @@ import org.universAAL.middleware.service.ServiceResponse;
 import org.universAAL.middleware.service.owl.InitialServiceDialog;
 import org.universAAL.middleware.service.owls.profile.ServiceProfile;
 import org.universAAL.ontology.profile.User;
-
-/**
- * ****************************************************************************
- * Copyright 2012 , http://www.prosyst.com - ProSyst Software GmbH
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ****************************************************************************
- */
 
 public class DispenserDisplayInstructionsDialogProvider extends ServiceCallee {
 
