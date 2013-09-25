17370f179a335f46dfe4c77daa5a6a62d83ec803
Miguel A. Llorente <mllorente@tsbtecnologias.es>
2013-09-25 15:47:46 +0000

    Changes for prototype

--- long_term_behav/ltba.manager/src/main/java/org/universAAL/ltba/service/LTBAProvider.java
+++ long_term_behav/ltba.manager/src/main/java/org/universAAL/ltba/service/LTBAProvider.java
@@ -1,6 +1,6 @@
 /*
 	Copyright 2008-2014 TSB, http://www.tsbtecnologias.es
-	TSB - Tecnologías para la Salud y el Bienestar
+	TSB - Tecnologï¿½as para la Salud y el Bienestar
 	
 	See the NOTICE file distributed with this work for additional 
 	information regarding copyright ownership
@@ -19,17 +19,32 @@
  */
 package org.universAAL.ltba.service;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
 import org.universAAL.ltba.manager.ConsequenceListener;
 import org.universAAL.middleware.container.ModuleContext;
 import org.universAAL.middleware.container.utils.LogUtils;
 import org.universAAL.middleware.service.CallStatus;
+import org.universAAL.middleware.service.DefaultServiceCaller;
 import org.universAAL.middleware.service.ServiceCall;
 import org.universAAL.middleware.service.ServiceCallee;
+import org.universAAL.middleware.service.ServiceCaller;
+import org.universAAL.middleware.service.ServiceRequest;
 import org.universAAL.middleware.service.ServiceResponse;
+import org.universAAL.middleware.util.Constants;
+import org.universAAL.ontology.ltba.LTBAService;
+import org.universAAL.ontology.profile.AssistedPerson;
 
 public class LTBAProvider extends ServiceCallee {
 	private ModuleContext mctx;
 	private ConsequenceListener listener;
+	private Timer restartTimer;
+	private int EIGHT_HOURS = 28800000;
+	private AssistedPerson inputUser = new AssistedPerson(
+			Constants.uAAL_MIDDLEWARE_LOCAL_ID_PREFIX + "saied");
 
 	public LTBAProvider(ModuleContext context) {
 		super(context, ProvidedLTBAService.profiles);
@@ -38,6 +53,7 @@ public class LTBAProvider extends ServiceCallee {
 
 	public LTBAProvider(ModuleContext mc, ConsequenceListener listener) {
 		super(mc, ProvidedLTBAService.profiles);
+		mctx = mc;
 		this.listener = listener;
 	}
 
@@ -62,21 +78,26 @@ public class LTBAProvider extends ServiceCallee {
 				return null;
 			} else if (operation
 					.startsWith(ProvidedLTBAService.SERVICE_SWITCH_ON)) {
-//				System.out.println("SWITCHING ON IN HANDLERe");
+				// System.out.println("SWITCHING ON IN HANDLERe");
 				LogUtils.logTrace(mctx, getClass(), "ServiceProvided",
 						new String[] { "SWITCHING ON..." }, null);
 				listener.setStatus(true);
 				return new ServiceResponse(CallStatus.succeeded);
 			} else if (operation
 					.startsWith(ProvidedLTBAService.SERVICE_SWITCH_OFF)) {
-//				System.out.println("SWITCHING OFF IN HANDLER");
-				LogUtils.logTrace(mctx, getClass(), "ServiceProvided",
-						new String[] { "SWITCHING OFF..." }, null);
+				// System.out.println("SWITCHING OFF IN HANDLER");
+				LogUtils.logTrace(
+						mctx,
+						getClass(),
+						"ServiceProvided",
+						new String[] { "SWITCHING OFF...THE LTBA WILL BE OPERATIVE AGAIN IN 8 HOURS" },
+						null);
 				listener.setStatus(false);
+				restartLTBA();
 				return new ServiceResponse(CallStatus.succeeded);
 			} else if (operation
 					.startsWith(ProvidedLTBAService.SERVICE_PRINT_REPORT)) {
-//				System.out.println("PRINTING REPORT!!");
+				// System.out.println("PRINTING REPORT!!");
 				// Assuming it's created in the Activator
 				ConsequenceListener.getInstance().printDayReport(
 						call.getInvolvedUser());
@@ -86,19 +107,44 @@ public class LTBAProvider extends ServiceCallee {
 				System.out.println("PRINTING WEEK!!");
 				// Assuming it's created in the Activator
 				/** Gonna print a week report for testing */
-				ConsequenceListener.getInstance().printWeekReport(call.getInvolvedUser());
+				ConsequenceListener.getInstance().printWeekReport(
+						call.getInvolvedUser());
 				return new ServiceResponse(CallStatus.succeeded);
 			} else if (operation
 					.startsWith(ProvidedLTBAService.SERVICE_SHOW_MONTH)) {
 				System.out.println("PRINTING MONTH!!");
 				// Assuming it's created in the Activator
 				/** Gonna print a week report for testing */
-				ConsequenceListener.getInstance().printMonthReport(call.getInvolvedUser());
+				ConsequenceListener.getInstance().printMonthReport(
+						call.getInvolvedUser());
 				return new ServiceResponse(CallStatus.succeeded);
 			}
 		}
 		return new ServiceResponse(CallStatus.serviceSpecificFailure);
 
+	}
+
+	private void restartLTBA() {
+
+		if (restartTimer != null) {
+			restartTimer.stop();
+		}
+		restartTimer = new Timer(EIGHT_HOURS, new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {				
+				ServiceRequest switchOn = new ServiceRequest(new LTBAService(),
+						inputUser);
+				switchOn.addAddEffect(
+						new String[] { LTBAService.PROP_SERVICE_HAS_STATUS_VALUE },
+						true);
+				ServiceCaller caller = new DefaultServiceCaller(mctx);
+				ServiceResponse sr = caller.call(switchOn);
+				System.out.println(sr.toString());
+				restartTimer.stop();
+				// listener.setStatus(true);
+			}
+		});
+		restartTimer.start();
 	}
 
 }
