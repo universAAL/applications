d2cf3e8b4504322d1e0c7c3062492d5da421ef84
Miguel A. Llorente <mllorente@tsbtecnologias.es>
2013-10-02 08:42:19 +0000

    Properties extracted from the code to a external file,

---
+++ long_term_behav/ltba.manager/src/main/java/org/universAAL/ltba/manager/Setup.java
@@ -0,0 +1,59 @@
+/*
+	Copyright 2011-2012 TSB, http://www.tsbtecnologias.es
+	TSB - Tecnolog�as para la Salud y el Bienestar
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ltba.manager;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.universAAL.middleware.container.osgi.util.BundleConfigHome;
+
+public class Setup {
+	public static final String HOME_FOLDER = "ltba.manager";
+	public static final String PROPERTIES_FILE = "ltba.properties";
+
+	private static String getSetupFileName() {
+		return new BundleConfigHome(HOME_FOLDER).getAbsolutePath();
+	}
+
+	static public String getServerIP() {
+		Properties properties = new Properties();
+		try {
+			String home = getSetupFileName();
+			properties.load(new FileInputStream(home + "/" + PROPERTIES_FILE));
+			return properties.getProperty("nomhad.ip");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	static public String getUserCode() {
+		Properties properties = new Properties();
+		try {
+			String home = getSetupFileName();
+			properties.load(new FileInputStream(home + "/" + PROPERTIES_FILE));
+			return properties.getProperty("nomhad.user.code");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
--- long_term_behav/ltba.manager/src/main/java/org/universAAL/ltba/pir/ActivityIndexController.java
+++ long_term_behav/ltba.manager/src/main/java/org/universAAL/ltba/pir/ActivityIndexController.java
@@ -2,16 +2,20 @@ package org.universAAL.ltba.pir;
 
 import org.universAAL.ltba.manager.CommonUtils;
 import org.universAAL.ltba.manager.ConsequenceListener;
+import org.universAAL.ltba.manager.Setup;
 import org.universAAL.middleware.container.utils.LogUtils;
 
 import es.tsb.ltba.nomhad.gateway.NomhadGateway;
 
-public class ActivityIndexController {
+public class ActivityIndexController extends PIRController {
 
 	private final String INDICATOR_GROUP = "INDEXES";
 	private final String INDICATOR = "ACTIVITY_LEVEL";
-	private String serverIp = "192.168.238.40";
-	private String userCode = "A100";
+	// private String serverIp = "192.168.238.40";
+	// private String userCode = "A100";
+	// private String serverIp = null;
+	// private String userCode = null;
+
 	private static ActivityIndexController INSTANCE;
 	/**
 	 * The device ID. When the user management be made, the DEVICE_ID must
@@ -25,14 +29,17 @@ public class ActivityIndexController {
 	private ActivityIndexController() {
 		super();
 		INSTANCE = this;
-		String ip = System.getProperty("es.tsbtecnologias.nomhad.server.ip");
-		String usr = System.getProperty("es.tsbtecnologias.nomhad.usercode");
-		if (ip != null) {
-			serverIp = ip;
-		}
-		if (usr != null) {
-			userCode = usr;
-		}
+
+		// String ip = System.getProperty("es.tsbtecnologias.nomhad.server.ip");
+		// String usr = System.getProperty("es.tsbtecnologias.nomhad.usercode");
+		// String ip = Setup.getServerURL();
+		// String usr = Setup.getUserCode();
+		// if (ip != null) {
+		// serverIp = ip;
+		// }
+		// if (usr != null) {
+		// userCode = usr;
+		// }
 	}
 
 	public static ActivityIndexController getInstance() {
@@ -43,13 +50,12 @@ public class ActivityIndexController {
 	}
 
 	public void addActivityIndex(String index) {
-		LogUtils
-				.logInfo(ConsequenceListener.getInstance().getModuleContext(),
-						getClass(), "addActivityIndex",
-						new String[] { "Sending to Nomhad >>> ActivityIndex: "
-								+ index }, null);
+		LogUtils.logInfo(ConsequenceListener.getInstance().getModuleContext(),
+				getClass(), "addActivityIndex",
+				new String[] { "Sending to Nomhad >FROM>FILE> ActivityIndex: "
+						+ index }, null);
 		System.out.println("Sending to Nomhad activityIndex>" + index);
-		NomhadGateway.getInstance().putMeasurement(serverIp, userCode,
+		NomhadGateway.getInstance().putMeasurement(serverIP, userCode,
 				CommonUtils.NOMHAD_DEVICE_CODE, INDICATOR_GROUP, INDICATOR,
 				new String(index), DEVICE_ID);
 	}
