7b91ad538649df0e2fe5da9925d2db4aefebfbd4
Miguel A. Llorente <mllorente@tsbtecnologias.es>
2013-10-18 09:36:21 +0000

    Changes for pilot...

--- long_term_behav/ltba.ui/src/main/java/org/universaal/ltba/ui/activator/MainLTBAUIProvider.java
+++ long_term_behav/ltba.ui/src/main/java/org/universaal/ltba/ui/activator/MainLTBAUIProvider.java
@@ -19,9 +19,20 @@
  */
 package org.universaal.ltba.ui.activator;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
+import java.util.Properties;
+
+import javax.swing.Timer;
 
 import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.util.BundleConfigHome;
+import org.universAAL.middleware.container.utils.LogUtils;
 import org.universAAL.middleware.owl.supply.LevelRating;
 import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.ui.UICaller;
@@ -30,6 +41,7 @@ import org.universAAL.middleware.ui.UIResponse;
 import org.universAAL.middleware.ui.owl.PrivacyLevel;
 import org.universAAL.middleware.ui.rdf.Form;
 import org.universAAL.middleware.ui.rdf.Label;
+import org.universAAL.middleware.ui.rdf.SimpleOutput;
 import org.universAAL.middleware.ui.rdf.Submit;
 import org.universAAL.middleware.util.Constants;
 import org.universAAL.ontology.profile.AssistedPerson;
@@ -38,29 +50,38 @@ import org.universaal.ltba.ui.impl.common.SharedResources;
 import org.universaal.ltba.ui.impl.reports.DayUI;
 import org.universaal.ltba.ui.impl.reports.MonthUI;
 import org.universaal.ltba.ui.impl.reports.WeekUI;
+
 /**
  * User interface provider.
+ * 
  * @author mllorente
- *
+ * 
  */
 public class MainLTBAUIProvider extends UICaller {
 
 	static final String MY_UI_NAMESPACE = "http://www.tsbtecnolgias.es/LTBAUICaller.owl#";
-	static final String SUBMISSION_ON = MY_UI_NAMESPACE + "submissionOn";
+	public static final String SUBMISSION_ON = MY_UI_NAMESPACE + "submissionOn";
 	static final String SUBMISSION_OFF = MY_UI_NAMESPACE + "submissionOff";
 	static final String SUBMISSION_SHOW = MY_UI_NAMESPACE + "submissionDay";
 	static final String SUBMISSION_WEEK = MY_UI_NAMESPACE + "submissionWeek";
 	static final String SUBMISSION_MONTH = MY_UI_NAMESPACE + "submissionMonth";
 
+	public final String HOME_FOLDER = "ltba.manager";
+	public final String PROPERTIES_FILE = "ltba.properties";
+	public final String PROPERTY_MAX_TIME_DISCONNECTED = "max.time.disconnected";
+
+	private static int timeToConnect = SharedResources.OFFTIME;
+	private static Timer disconnectionTimer;
+
 	static final String USER_INPUT_SELECTED_RANGE = MY_UI_NAMESPACE
 			+ "selectedRange";
 
-	ModuleContext myModuleContext;
-	Resource lastUser = new Resource();
+	static ModuleContext myModuleContext;
 
 	public MainLTBAUIProvider(ModuleContext context) {
 		super(context);
 		myModuleContext = context;
+		SharedResources.context = context;
 	}
 
 	@Override
@@ -74,33 +95,34 @@ public class MainLTBAUIProvider extends UICaller {
 	}
 
 	public Form showDialog(Resource inputUser) {
-		lastUser = inputUser;
+		SharedResources.lastUser = inputUser;
 		Form f = Form.newDialog("LTBA UI", new Resource());
-		// start of the form model
-		// new SimpleOutput(f.getIOControls(), null, null,
-		// "Long Term Behaviour Analyzer User Interface");
+		new SimpleOutput(f.getIOControls(), null, null,
+				SharedResources.getMainMessage());
 		// new SimpleOutput(f.getIOControls(), null, null, "NANONANO");
 		// new SimpleOutput(f.getIOControls(), null, null, "TETETETE");
 
 		// Submit s = new Submit(f.getIOControls(), new Label("Activate LTBA",
 		// null), "ltbaButton");
+
 		if (SharedResources.ltbaIsOn) {
-			new Submit(f.getIOControls(), new Label(">ltbaOn<", null),
+			new Submit(f.getIOControls(), new Label(">LTBA ON<", null),
 					SUBMISSION_ON);
-			new Submit(f.getIOControls(), new Label("ltbaOff", null),
+			new Submit(f.getIOControls(), new Label("LTBA OFF", null),
 					SUBMISSION_OFF);
 		} else {
-			new Submit(f.getIOControls(), new Label("ltbaOn", null),
+
+			new Submit(f.getIOControls(), new Label("LTBA ON", null),
 					SUBMISSION_ON);
-			new Submit(f.getIOControls(), new Label(">ltbaOff<", null),
+			new Submit(f.getIOControls(), new Label(">LTBA OFF<", null),
 					SUBMISSION_OFF);
 		}
-//		new Submit(f.getSubmits(), new Label("Day Report", null),
-//				SUBMISSION_SHOW);
-//		new Submit(f.getSubmits(), new Label("Week Report", null),
-//				SUBMISSION_WEEK);
-//		new Submit(f.getSubmits(), new Label("Month Report", null),
-//				SUBMISSION_MONTH);
+		// new Submit(f.getSubmits(), new Label("Day Report", null),
+		// SUBMISSION_SHOW);
+		// new Submit(f.getSubmits(), new Label("Week Report", null),
+		// SUBMISSION_WEEK);
+		// new Submit(f.getSubmits(), new Label("Month Report", null),
+		// SUBMISSION_MONTH);
 		if (inputUser == null) {
 			System.out.println("NULL INPUT USER");
 		}
@@ -120,49 +142,135 @@ public class MainLTBAUIProvider extends UICaller {
 		UIRequest req = new UIRequest(inputUser, f, LevelRating.none,
 				Locale.ENGLISH, PrivacyLevel.insensible);
 		this.sendUIRequest(req);
+		SharedResources.thisform = f;
 		return f;
 	}
 
 	public void handleUIResponse(UIResponse uir) {
-		if (lastUser == null) {
-			lastUser = new AssistedPerson(
+		if (SharedResources.lastUser == null) {
+			SharedResources.lastUser = new AssistedPerson(
 					Constants.uAAL_MIDDLEWARE_LOCAL_ID_PREFIX + "saied");
 		}
 		if (uir != null) {
 			if (SUBMISSION_ON.equals(uir.getSubmissionID())) {
-				SharedResources.ltbaIsOn = true;
-				new LTBACaller(myModuleContext, lastUser);
+				LogUtils.logDebug(myModuleContext, getClass(),
+						"handleUIResponse", "SUBMISSION_ON");
+				switchOnOperations();
+				new LTBACaller(myModuleContext, SharedResources.lastUser);
 				LTBACaller.switchOn();
 			} else if (SUBMISSION_OFF.equals(uir.getSubmissionID())) {
-				SharedResources.ltbaIsOn = false;
-				new LTBACaller(myModuleContext, lastUser);
+				LogUtils.logDebug(myModuleContext, getClass(),
+						"handleUIResponse", "SUBMISSION_OFF");
+				switchOffOperations(this);
+				new LTBACaller(myModuleContext, SharedResources.lastUser);
 				LTBACaller.switchOff();
-			} else if (SUBMISSION_SHOW.equals(uir.getSubmissionID())) {
-				myModuleContext.logDebug("","To DroolsCaller in SUBMISSION_SHOW",
-						null);
-				new DayUI(myModuleContext).showDialog(lastUser);
-				return;
-			} else if (SUBMISSION_WEEK.equals(uir.getSubmissionID())) {
-				myModuleContext.logDebug("","To DroolsCaller in SUBMISSION_WEEK",
-						null);
-				new WeekUI(myModuleContext).showDialog(lastUser);
-				return;
-			} else if (SUBMISSION_MONTH.equals(uir.getSubmissionID())) {
-				myModuleContext.logDebug("","To DroolsCaller in SUBMISSION_MONTH",
-						null);
-				new MonthUI(myModuleContext).showDialog(lastUser);
-				return;
 			}
 			// yoda conditions everywhere
 			if ("doneForm".equals(uir.getSubmissionID())) {
-				startMainDialog();
+				// startMainDialog();
 			} else {
-				showDialog(lastUser);
+				showDialog(SharedResources.lastUser);
 			}
 		}
 	}
 
-	void startMainDialog() {
+	public void switchOffOperations(MainLTBAUIProvider mainLTBAUIProvider) {
+		initDisconnectionPeriod();
+		SharedResources.ltbaIsOn = false;
+		setReconnectionTime();
 	}
 
+	public void switchOnOperations() {
+		disconnectionTimer.stop();
+		disconnectionTimer = null;
+		timeToConnect = SharedResources.OFFTIME;
+		SharedResources.ltbaIsOn = true;
+	}
+
+	/**
+	 * Set the re-connection time. Configures the hour shown to the user.
+	 */
+	private void setReconnectionTime() {
+
+		Date disconnection = new Date();
+		Date reconnection = new Date(disconnection.getTime()
+				+ (1000 * 60 * getTimeDisconected()));
+		LogUtils.logDebug(myModuleContext, getClass(), "setReconnectionTime",
+				"DISCONNECTION->" + disconnection);
+		LogUtils.logDebug(myModuleContext, getClass(), "setReconnectionTime",
+				"RECONNECTION->" + reconnection);
+		Calendar disconnectedTime = Calendar.getInstance();
+		disconnectedTime.setTime(disconnection);
+		Calendar reconnectionTime = Calendar.getInstance();
+		reconnectionTime.setTime(reconnection);
+		int recHour = reconnectionTime.get(Calendar.HOUR_OF_DAY);
+		int recMinutes = reconnectionTime.get(Calendar.MINUTE);
+		String recTimeFormatted;
+		if (recHour < 10) {
+			recTimeFormatted = "0" + recHour;
+		} else {
+			recTimeFormatted = "" + recHour;
+		}
+		if (recMinutes < 10) {
+			recTimeFormatted = recTimeFormatted + ":0" + recMinutes;
+		} else {
+			recTimeFormatted = recTimeFormatted + ":" + recMinutes;
+		}
+		SharedResources.reconnectHour = recTimeFormatted;
+
+		if (reconnectionTime.get(Calendar.DAY_OF_MONTH) > disconnectedTime
+				.get(Calendar.DAY_OF_MONTH)) {
+			SharedResources.reconnectToday = false;
+		} else {
+			SharedResources.reconnectToday = true;
+		}
+	}
+
+	/**
+	 * Timer that will reconnect automatically the LTBA service after some
+	 * defined hours.
+	 */
+	public void initDisconnectionPeriod() {
+		/**
+		 * Timer update period -> 1 minute timeToConnect defines the number of
+		 * minutes remaining for the reconnection
+		 */
+		disconnectionTimer = new Timer(60 * 1000, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LogUtils.logDebug(myModuleContext, getClass(),
+						"actionPerformed", "timeToConnect->" + timeToConnect
+								+ " mintues");
+				if (timeToConnect == 0) {
+					new LTBACaller(myModuleContext, null);
+					SharedResources.ltbaIsOn = true;
+					LTBACaller.switchOn();
+					UIResponse uir = new UIResponse();
+					// NEW WINDOW WITH LTBA ON
+					handleUIResponse(new UIResponse(SharedResources.lastUser,
+							null, new Submit(SharedResources.thisform
+									.getIOControls(), new Label(">LTBA ON<",
+									null), SUBMISSION_ON)));
+					SharedResources.ltbaIsOn = true;
+					showDialog(SharedResources.lastUser);
+					disconnectionTimer.stop();
+					timeToConnect = SharedResources.OFFTIME;
+				}
+				timeToConnect--;// 1 minute left
+			}
+		});
+		disconnectionTimer.start();
+	}
+
+	private long getTimeDisconected() {
+		String home = new BundleConfigHome(HOME_FOLDER).getAbsolutePath();
+		Properties properties = new Properties();
+		try {
+			properties.load(new FileInputStream(home + "/" + PROPERTIES_FILE));
+			return Long.parseLong(properties
+					.getProperty(PROPERTY_MAX_TIME_DISCONNECTED));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }
--- long_term_behav/ltba.ui/src/main/java/org/universaal/ltba/ui/impl/common/SharedResources.java
+++ long_term_behav/ltba.ui/src/main/java/org/universaal/ltba/ui/impl/common/SharedResources.java
@@ -1,6 +1,6 @@
 /*
 	Copyright 2008-2014 TSB, http://www.tsbtecnologias.es
-	TSB - Tecnologías para la Salud y el Bienestar
+	TSB - Tecnologï¿½as para la Salud y el Bienestar
 	
 	See the NOTICE file distributed with this work for additional 
 	information regarding copyright ownership
@@ -19,67 +19,123 @@
  */
 package org.universaal.ltba.ui.impl.common;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.Timer;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.universAAL.middleware.container.ModuleContext;
-import org.universaal.ltba.ui.activator.MainLTBAUIProvider;
+import org.universAAL.middleware.container.osgi.util.BundleConfigHome;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.ui.rdf.Form;
 
 public class SharedResources {
 
 	public static final String DROOLS_UI_NAMESPACE = "http://www.tsbtecnologias.es/DroolsUI.owl#";
-	public static ModuleContext moduleContext;
-	static LTBAUIProvider serviceProvider;
 	public static boolean ltbaIsOn = true;
 	public static boolean processingReport = false;
+	public static Resource lastUser = new Resource();
+	public static ModuleContext context;
+	public static String reconnectHour;
 	/**
 	 * Period (in minutes) off switching off. After that the LTBA will
 	 * automatically restart with normal operation.
 	 */
-	public static int OFFTIME = 5; // 8 hours
+	public static int OFFTIME = getOfftime(); // minutes
+	public static Form thisform = null;
+	public static boolean reconnectToday = true;
 	/**
 	 * Time (in minutes) to finish the disconnection period.
 	 */
-	private int timeToConnect = OFFTIME;
-	private Timer disconnectionTimer;
 
-	// static ServiceProvider serviceProvider;
-	static MainLTBAUIProvider uIProvider;
+	public static final String HOME_FOLDER = "ltba.manager";
+	public static final String PROPERTIES_FILE = "ltba.properties";
+	public static final String MESSAGES_FILE = "ltba.messages";
 
-	// static final AssistedPerson testUser = new AssistedPerson(
-	// Constants.uAAL_MIDDLEWARE_LOCAL_ID_PREFIX + "saied");
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
-	 * )
-	 */
-	public void start() throws Exception {
-		new Thread() {
-			public void run() {
-				SharedResources.serviceProvider = new LTBAUIProvider(
-						moduleContext);
-				SharedResources.uIProvider = new MainLTBAUIProvider(
-						moduleContext);
-			}
-		}.start();
+	private static String getSetupFileName() {
+		return new BundleConfigHome(HOME_FOLDER).getAbsolutePath();
 	}
 
-	public void initDisconnectionPeriod() {
-		disconnectionTimer = new Timer(60 * 1000, new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (timeToConnect == 0) {
-					new LTBACaller(moduleContext, null);
-					LTBACaller.switchOn();
+	private static int getOfftime() {
+		Properties properties = new Properties();
+		try {
+			String home = getSetupFileName();
+			properties.load(new FileInputStream(home + "/" + PROPERTIES_FILE));
+			return Integer.parseInt(properties
+					.getProperty("max.time.disconnected"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return 0;
+	}
+
+	static public String getDisconnectedTimeString() {
+		Properties properties = new Properties();
+		try {
+			String home = getSetupFileName();
+			properties.load(new FileInputStream(home + "/" + PROPERTIES_FILE));
+			return properties.getProperty("max.time.disconnected");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	static public String getLanguage() {
+		Properties properties = new Properties();
+		try {
+			String home = getSetupFileName();
+			properties.load(new FileInputStream(home + "/" + PROPERTIES_FILE));
+			return properties.getProperty("language");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	static public String getMainMessage() {
+		String lan = getLanguage();
+		String home = getSetupFileName();
+		LogUtils.logDebug(context, SharedResources.class, "getMainMessage", lan);
+		Properties properties = new Properties();
+		try {
+			properties.load(new FileInputStream(home + "/" + MESSAGES_FILE));
+			LogUtils.logDebug(context, SharedResources.class, "getMainMessage",
+					"LTBA is ON ->" + ltbaIsOn);
+			if (ltbaIsOn) {
+				LogUtils.logDebug(context, SharedResources.class,
+						"getMainMessage", "ASKING FOR ltba.ui.connected.main."
+								+ lan.toLowerCase());
+				LogUtils.logDebug(
+						context,
+						SharedResources.class,
+						"getMainMessage",
+						properties.getProperty("ltba.ui.connected.main."
+								+ lan.toLowerCase()));
+				return properties.getProperty("ltba.ui.connected.main."
+						+ lan.toLowerCase());
+			} else {
+				if (reconnectToday) {
+					return properties.getProperty("ltba.ui.disconnected.main."
+							+ lan.toLowerCase())
+							+ reconnectHour+" "
+							+ properties
+									.getProperty("ltba.ui.disconnected.today."
+											+ lan.toLowerCase());
+				} else {
+					return properties.getProperty("ltba.ui.disconnected.main."
+							+ lan.toLowerCase())
+							+ reconnectHour+" "
+							+ properties
+									.getProperty("ltba.ui.disconnected.tomorrow."
+											+ lan.toLowerCase());
 				}
-				timeToConnect--;// 1 minute left
 			}
-		});
-		disconnectionTimer.start();
-	}
 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
