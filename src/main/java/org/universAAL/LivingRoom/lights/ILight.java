78d536d53e6e3cfbcce5ea071536c947ae4d74ec
Alvaro Fides <alfiva@itaca.upv.es>
2013-11-26 09:17:24 +0000

    Removed workout_at_home stuff that should have never been put at trunk level

--- src/main/java/org/universAAL/LivingRoom/lights/ILight.java
+++
@@ -1,8 +0,0 @@
-package org.universAAL.LivingRoom.lights;
-
-public interface ILight {
-	
-	boolean On();
-	void Off();
-	
-}
--- src/main/java/org/universAAL/LivingRoom/lights/provider/hue/HueRegisterStates.java
+++
@@ -1,7 +0,0 @@
-package org.universAAL.LivingRoom.lights.provider.hue;
-
-public enum HueRegisterStates {
-	Registered,
-	Error,
-	Unregistered
-}
