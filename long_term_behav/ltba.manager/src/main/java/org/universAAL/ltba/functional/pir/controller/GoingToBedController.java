e5ed36a1b69895f4f828ab23ad2cebb8ca2762f1
Miguel A. Llorente <mllorente@tsbtecnologias.es>
2013-12-03 13:19:38 +0000

    Version chanche to E.3.1, 2.0.3. Changes for pilot (better reasoning).

--- long_term_behav/ltba.manager/src/main/java/org/universAAL/ltba/functional/pir/controller/GoingToBedController.java
+++ long_term_behav/ltba.manager/src/main/java/org/universAAL/ltba/functional/pir/controller/GoingToBedController.java
@@ -20,16 +20,16 @@ public class GoingToBedController extends LTBAController implements
 	private Timer t;
 
 	/**
-	 * The device ID. When the user management will be implemented, the DEVICE_ID must
-	 * content a reference to the user, in order  not to using the same device
-	 * with different users. (TODO).
+	 * The device ID. When the user management will be implemented, the
+	 * DEVICE_ID must content a reference to the user, in order not to using the
+	 * same device with different users. (TODO).
 	 */
 	private final String DEVICE_ID = "BEDROOM_DET";
 
 	private GoingToBedController() {
 		super();
 		INSTANCE = this;
-		//CHECK THIS AT 23.45 PM
+		// CHECK THIS AT 23.45 PM
 		t = new Timer(24 * 60 * 60 * 1000, this);
 		Calendar today = new GregorianCalendar();
 		Calendar startTime = new GregorianCalendar(today.get(Calendar.YEAR),
@@ -56,9 +56,21 @@ public class GoingToBedController extends LTBAController implements
 	public void actionPerformed(ActionEvent e) {
 		System.out.println("Sending to Nomhad time>" + gtbTime + " and times>"
 				+ times);
-		NomhadGateway.getInstance().putMeasurement(serverIP, userCode,
-				userPassword, "SLEEPING", "GOING_TO_BED", new String("" + gtbTime),
-				DEVICE_ID);
+		// This condition ensures that the going to bed graph is over the waking
+		// up time.
+		// gtbtime = 01.00am -> 25 ; gtbtime = 03.00am -> 27 and so on...
+		if (gtbTime < 8) {
+			gtbTime = gtbTime + 24;
+		}
+//		NomhadGateway.getInstance().putMeasurement(serverIP, userCode,
+//				userPassword, "SLEEPING", "GOING_TO_BED",
+//				new String("" + gtbTime), DEVICE_ID);
+		NomhadGateway.getInstance().putMeasurement(serverIP,
+				Calendar.getInstance().get(Calendar.DAY_OF_MONTH),
+				Calendar.getInstance().get(Calendar.MONTH),
+				Calendar.getInstance().get(Calendar.YEAR), 12, 00, userCode,
+				userPassword, "SLEEPING", "GOING_TO_BED",
+				new String("" + gtbTime), DEVICE_ID);
 		TimeSleepingController.setGTBTime((float) gtbTime);
 		gtbTime = 0;
 	}
