805c2cad7b97fb97f462222b3754e9740ab1ed6b
Alvaro Fides <alfiva@itaca.upv.es>
2013-12-07 17:07:05 +0000

    tag to save hwo.mobile

---
+++ help_when_out/hwo.mobile/src/main/java/org/universAAL/AALapplication/hwo/Messages.java
@@ -0,0 +1,29 @@
+package org.universAAL.AALapplication.hwo;
+//Class that provides String messages in different languages.
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Messages {
+
+	private static ResourceBundle RESOURCE_BUNDLE;
+	
+	static{
+		try{
+		RESOURCE_BUNDLE = ResourceBundle.getBundle("org.universAAL.AALapplication.hwo.messages",Locale.getDefault());
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
