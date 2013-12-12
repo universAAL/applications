56d2eb8d86f113b7d14c91a980619b6de8b6c838
Ms Isabel Marti <imarti@tsbtecnologias.es>
2013-12-12 15:53:49 +0000

    Example of Follow Me

---
+++ FollowMeExamples/FollowMeExampleGUI/src/main/java/org/universAAL/AALapplication/FollowMeExampleGUI/Activator.java
@@ -0,0 +1,30 @@
+package org.universAAL.AALapplication.FollowMeExampleGUI;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+
+public class Activator implements BundleActivator {
+	public static BundleContext osgiContext = null;
+	public static ModuleContext context = null;
+	public static LocationSubscriber csubscriber = null;
+
+	public void start(BundleContext bcontext) throws Exception {
+		System.out.print("Starting ZWave Info Consumer");
+		Activator.osgiContext = bcontext;
+		Activator.context = uAALBundleContainer.THE_CONTAINER
+			.registerModule(new Object[] { bcontext });
+			//scallee=new AALfficiencyProvider(context);
+			new Thread() {
+			    public void run() {
+			    	new LocationSubscriber(context);
+			    }
+			}.start();
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+		csubscriber.close();
+	}
+
+}
